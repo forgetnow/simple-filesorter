a19aba3acc7fc54ca7c73910d6ebea695419c023
forgetnow <99139245+forgetnow@users.noreply.github.com>
2022-02-13 12:12:42 +0300

    Exist file saved and new exeption lock

--- Program.cs
+++ Program.cs
@@ -6,89 +6,111 @@ using System.Threading;
 
 class Program
 {
-    internal static FileInfo info;
     internal static Dictionary<string, List<string>> types = new Dictionary<string, List<string>>();
+    static string mainPath = "V:\\Files";
 
-    static string FilesDirectory = @"C:\Users\Admin\Desktop\Files";
-
-    private static void Main(string[] args)
+    private static void Main()
     {
-        if (!Directory.Exists(FilesDirectory))
+        if (!Directory.Exists(mainPath))
         {
-            Directory.CreateDirectory(FilesDirectory);
+            Directory.CreateDirectory(mainPath);
         }
 
-        types.Add("Images", new List<string>() { ".png", ".jpeg", ".bmp", ".gif", ".jpg", ".mpo", ".png", ".arw4" });
-        types.Add("Music", new List<string>() { ".mp3", ".wav", ".m4a", ".flac", ".mp3", ".wma", ".aac" });
-        types.Add("Video", new List<string>() { ".mp4", ".avi", ".flv", ".wmv", ".mkv", ".mpg", ".mpe", ".mpeg", ".m2ts", ".mts", ".divx", ".asf", ".wmv", ".divx", ".ogv", ".3gp", ".3g2", ".rm", ".rmvb", ".mov", ".flv", ".mvc" });
-        types.Add("Document", new List<string>() { ".doc", ".ppt", ".xls", ".txt", ".pdf" });
-        types.Add("Archive", new List<string>() { ".zip", ".rar", ".7z", ".tar", ".gzip", ".bzip2", ".apk", ".ipa", ".nrg" });
-        types.Add("Executable", new List<string>() { ".exe", ".bat", ".com", ".jar", ".iso" });
-        types.Add("Trash", new List<string>() { ".torrent", ".lnk" });
+        types.Add("Images", new List<string>() { "png", "jpeg", "bmp", "gif", "jpg", "mpo", "png", "arw4" });
+        types.Add("Music", new List<string>() { "mp3", "wav", "m4a", "flac", "mp3", "wma", "aac" });
+        types.Add("Video", new List<string>() { "mp4", "avi", "flv", "wmv", "mkv", "mpg", "mpe", "mpeg", "m2ts", "mts", "divx", "asf", "wmv", "divx", "ogv", "3gp", "3g2", "rm", "rmvb", "mov", "flv", "mvc" });
+        types.Add("Document", new List<string>() { "doc", "ppt", "xls", "txt", "pdf" });
+        types.Add("Archive", new List<string>() { "zip", "rar", "7z", "tar", "gzip", "bzip2", "apk", "ipa", "nrg" });
+        types.Add("Executable", new List<string>() { "exe", "bat", "com", "jar", "iso" });
+        types.Add("Trash", new List<string>() { "torrent", "lnk" });
 
         foreach (string folder in types.Keys)
         {
-            if (!Directory.Exists(FilesDirectory + @"\" + folder))
+            if (!Directory.Exists(mainPath + "\\" +  folder))
             {
-                Directory.CreateDirectory(FilesDirectory + @"\" + folder);
+                Directory.CreateDirectory(mainPath + "\\" + folder);
             }
         }
 
-        if (!Directory.Exists(FilesDirectory + "//Other"))
+        if (!Directory.Exists(mainPath + "\\Other"))
         {
-            Directory.CreateDirectory(FilesDirectory + "//Other");
+            Directory.CreateDirectory(mainPath + "\\Other");
         }
 
-        FileSystemWatcher watcher = new FileSystemWatcher(FilesDirectory, "*.*");
+        FileSystemWatcher watcher = new FileSystemWatcher(mainPath, "*.*");
         watcher.IncludeSubdirectories = false;
         watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Attributes | NotifyFilters.CreationTime;
-        watcher.Created += new FileSystemEventHandler(SortFile);
+        watcher.Created += sortFile;
         watcher.EnableRaisingEvents = true;
 
-        while (true) { };
+        while (true) { }
     }
 
-    private static void SortFile(object sender, FileSystemEventArgs CurrentFile)
+    private static void sortFile(object sender, FileSystemEventArgs CurrentFile)
     {
-#if debug
-        Console.WriteLine("Sort Process");
-#endif
-        info = new FileInfo(CurrentFile.FullPath);
-
-        foreach (var type in types)
+        FileInfo fileInfo = new FileInfo(CurrentFile.FullPath);
+         
+        foreach (KeyValuePair<string, List<string>> type in types)
         {
-#if debug
-            Console.WriteLine("1 step");
-#endif
-            foreach (var name in type.Value)
+            foreach (string name in type.Value)
             {
-#if debug
-                Console.WriteLine(name);
-#endif
-                if (name == info.Extension)
+                Console.WriteLine("." + name);
+                if ("." + name == fileInfo.Extension)
                 {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        try
-                        {
-#if debug
-                            Console.WriteLine(FilesDirectory + "\\" + type.Key.ToString() + "\\");
-                            info.MoveTo(FilesDirectory + "\\" + type.Key.ToString() + "\\" + info.Name);
-                            break;
-#endif
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                            Thread.Sleep(1000);
-                            continue;
-                        }
-                    }
+                    moveFile(fileInfo, type.Key.ToString());
                     return;
                 }
             }
-
-            info.MoveTo(FilesDirectory + "\\Other\\" + info.Name);
         }
+
+        moveFile(fileInfo, "Other");
+    }
+
+    private static void moveFile(FileInfo fileInfo, string type)
+    {
+        try
+        {
+            string newName = Path.GetFileNameWithoutExtension(fileInfo.FullName);
+            string newPath = mainPath + "\\" + type + "\\";
+
+            if (File.Exists(newPath + fileInfo.Name))
+            {
+                newName = getRenamedFile(fileInfo, newPath);
+            }
+
+            fileInfo.MoveTo(newPath + newName + fileInfo.Extension);
+            return;
+        }
+        catch (FileNotFoundException)
+        {
+            return;
+        }
+        catch (IOException)
+        {
+            Thread.Sleep(1000);
+
+            moveFile(fileInfo, type);
+        }
+        catch
+        {
+            return;
+        }
+    }
+
+    private static string getRenamedFile(FileInfo fileInfo, string path)
+    {
+        string currentName;
+        int filecount = 1;
+
+        do
+        {
+            currentName = Path.GetFileNameWithoutExtension(fileInfo.FullName);
+            currentName = currentName + "(" + filecount + ")";
+            filecount++;
+        }
+        while (File.Exists(path + currentName + fileInfo.Extension));
+
+        Console.WriteLine(currentName);
+        return currentName;
     }
 }
